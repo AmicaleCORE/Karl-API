e236af89804739d3d6f2614cb47ed115e3cbf66f
loicmaes <production@maesloic.fr>
2023-02-13 05:58:46 +0100

    :rocket: Transaction models

---
+++ src/database/models/comptability/transactions/Transaction.ts
@@ -0,0 +1,53 @@
+import {DataTypes, Sequelize} from "sequelize";
+
+module.exports = (orm: Sequelize) => {
+    return orm.define(``, {
+        code: {
+            type: DataTypes.STRING(15),
+            primaryKey: true
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        billing_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        flow: {
+            type: DataTypes.ENUM,
+            values: Object.values(TransactionFlow)
+        },
+        source: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        target: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        amount: {
+            type: DataTypes.FLOAT,
+            validate: {
+                min: 0
+            }
+        },
+        correction_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        }
+    }, {
+        timestamps: true,
+        createdAt: true,
+        updatedAt: false,
+        deletedAt: false
+    })
+}
---
+++ src/database/models/comptability/transactions/corrections/TransactionCorrection.ts
@@ -0,0 +1,25 @@
+import {DataTypes, Sequelize} from "sequelize";
+
+module.exports = (orm: Sequelize) => {
+    return orm.define('transaction_correction', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        type_id: {
+            type: DataTypes.INTEGER
+        },
+        transaction_code: {
+            type: DataTypes.STRING
+        },
+        reason: {
+            type: DataTypes.TEXT
+        }
+    }, {
+        timestamps: false,
+        createdAt: false,
+        updatedAt: false,
+        deletedAt: false
+    })
+}
