e974c1aa79002fd6adc71f96829e34aac9eefe65
loicmaes <production@maesloic.fr>
2023-02-13 15:14:50 +0100

    :rocket: Use handlers and database

---
+++ src/App.ts
@@ -0,0 +1,19 @@
+import {initDatabase} from "~/database/Orm";
+import {Express} from "express";
+
+require('dotenv')
+const express = require('express')
+
+const app: Express = express()
+
+// Default middlewares
+app
+    .use(express.json()) // Body to JSON
+
+require('~/framework/handlers/RouteHandler')(app)
+
+const port: number = parseInt(process.env.PORT || '3000')
+app.listen(port, () => {
+    initDatabase(false)
+    console.log(`Listening on: http://localhost:${port}/`)
+})
