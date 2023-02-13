8c7235f06ee564397a50a474091b78775c21bd25
loicmaes <production@maesloic.fr>
2023-02-13 15:14:37 +0100

    :rocket: Database entry point

---
+++ src/database/Orm.ts
@@ -0,0 +1,18 @@
+import {Sequelize} from "sequelize";
+
+const orm: Sequelize = new Sequelize({
+    storage: 'database.db',
+    dialect: 'sqlite',
+    dialectOptions: {
+        timezone: 'Etc/GMT-2'
+    },
+    logging: false
+})
+
+export const models = require('~/framework/handlers/ModelHandler')(orm)
+
+export function initDatabase(force: boolean) {
+    orm.sync({ force }).then(_ => {
+        console.log(`Database synchronized.`)
+    }).catch((err: Error) => console.log(err))
+}
