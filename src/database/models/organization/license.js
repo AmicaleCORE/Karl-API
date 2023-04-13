52e7b24e580a4f7e5dcf4b9e519c08e3c26d1c2b
Czekaj Tom <red57101@gmail.com>
2023-04-13 12:14:45 +0200

    :rocket: Add license table

---
+++ src/database/models/organization/license.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+    const License = sequelize.define("license", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        key: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+            unique: true,
+        },
+        mail: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+        expiration_date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        }
+    },
+    {
+        underscored: true,
+        createdAt: true,
+        updatedAt: true
+    });
+    return License;
+};
