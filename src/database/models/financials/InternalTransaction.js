7a4c935b429cfd0a93607eb83635f6b2c21408de
Czekaj Tom <red57101@gmail.com>
2023-04-13 14:37:02 +0200

    :sparkles: Implement internal_transactions table

---
+++ src/database/models/financials/InternalTransaction.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require("sequelize");
+const {getModel} = require("../../../handlers/modelsHandler");
+
+module.exports = (sequelize) => {
+    const InternalTransaction = sequelize.define("internal_transaction", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        transaction_type: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        account_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        debit_amount: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false,
+            defaultValue: 0
+        },
+        credit_amount: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false,
+            defaultValue: 0
+        }
+    },
+    {
+        underscored: true,
+        createdAt: true,
+        updatedAt: true
+    });
+    const InternalAccount = getModel(sequelize, "financials/internalAccount.js");
+    InternalTransaction.belongsTo(InternalAccount, {foreignKey: "account_id"});
+    return InternalTransaction;
+};
