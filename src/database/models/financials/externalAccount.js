eb7ec4f25d44b458d9d0d71248cc2725ed561b53
Czekaj Tom <red57101@gmail.com>
2023-04-16 00:57:18 +0200

    :art: Fix indentation

--- src/database/models/financials/externalAccount.js
+++ src/database/models/financials/externalAccount.js
@@ -3,23 +3,23 @@ const {getModel} = require("../../../handlers/modelsHandler");
 
 module.exports = (sequelize) => {
     return sequelize.define("external_account", {
-            id: {
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true
-            },
-            organization_id: {
-                type: DataTypes.INTEGER,
-                allowNull: false
-            },
-            name: {
-                type: DataTypes.STRING(255),
-                allowNull: false
-            }
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
         },
-        {
-            underscored: true,
-            createdAt: true,
-            updatedAt: false
-        });
+        organization_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        }
+    },
+    {
+        underscored: true,
+        createdAt: true,
+        updatedAt: false
+    });
 };
