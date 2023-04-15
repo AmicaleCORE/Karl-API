487de775c91dc9517f73b0f03c6e95b597075044
Czekaj Tom <red57101@gmail.com>
2023-04-16 01:36:32 +0200

    :sparkles: Add minor_inventory_corrections table

---
+++ src/database/models/products/minorInventoryCorrection.js
@@ -0,0 +1,40 @@
+const {DataTypes} = require("sequelize");
+const {getModel} = require("../../../handlers/modelsHandler");
+
+module.exports = (sequelize) => {
+    const MinorInventoryCorrection = sequelize.define("minor_inventory_correction", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        organization_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        product_transaction_code: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        correction_reason_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        state: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        }
+    },
+    {
+        underscored: true,
+        createdAt: true,
+        updatedAt: false
+    });
+    const Organization = getModel(sequelize, "organizations/organization.js");
+    const ProductTransaction = getModel(sequelize, "products/productTransaction.js");
+    const CorrectionReason = getModel(sequelize, "products/correctionReason.js");
+    MinorInventoryCorrection.belongsTo(Organization, {foreignKey: "organization_id"});
+    MinorInventoryCorrection.belongsTo(ProductTransaction, {foreignKey: "product_transaction_code"});
+    MinorInventoryCorrection.belongsTo(CorrectionReason, {foreignKey: "correction_reason_id"});
+    return MinorInventoryCorrection;
+};
