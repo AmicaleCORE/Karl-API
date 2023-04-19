dbdfab5bcd4b20ec7e2323079d5e9ef3ea991216
Czekaj Tom <red57101@gmail.com>
2023-04-20 00:33:49 +0200

    :sparkles: Add routesHandler.js

---
+++ src/handlers/routesHandler.js
@@ -0,0 +1,14 @@
+const loadFiles = require('./filesHandler');
+
+module.exports = (router) => {
+    console.log('------ Loading routes ------');
+    const files = loadFiles('./src/rest/routes', true);
+    files.forEach(file => {
+        try{
+            require(`../rest/routes/${file}`)(router);
+            console.log(`✅  Route ${file} registered!`);
+        } catch (e){
+            console.error(`❌  Error while registering route ${file}: ${e}`);
+        }
+    })
+}
