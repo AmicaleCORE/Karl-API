85f08e82792d15a6d3655337656e27b4f496aaa5
Czekaj Tom <red57101@gmail.com>
2023-04-20 16:15:36 +0200

    :sparkles: Add route to cancel a subscription

--- src/rest/routes/licenseRoutes.js
+++ src/rest/routes/licenseRoutes.js
@@ -1,15 +1,20 @@
 const licenseController = require("../controllers/licenseController");
 
 module.exports = (router) => {
+    // Create a checkout session
     router.post("/license/checkout", async (req, res) => {
         await licenseController.createCheckoutSession(req, res);
     });
-    // Check license validity
+    // Verify a payment and create a license
     router.post("/license/checkout/verify", async (req, res) => {
         await licenseController.verifyPayment(req, res);
     });
     // Check if a license is valid
     router.get("/license/:license", async (req, res) => {
         await licenseController.verifyValidity(req, res);
+    });
+    // Cancel a subscription
+    router.delete("/license/:license", async (req, res) => {
+        await licenseController.cancelSubscription(req, res);
     });
 }
